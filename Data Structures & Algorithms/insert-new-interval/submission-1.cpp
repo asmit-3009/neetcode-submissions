0a4f4890e2b5759cab3cd2c8bf40c2c34d7104cc
Asmit Biswal <asmitbiswal05@gmail.com>
2026-07-11 23:52:31 +0530

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int newS = newInterval[0];
+        int newE = newInterval[1];
+        int n = intervals.size();
+        for(int i = 0; i < n; i++){
+            if(intervals[i][0] > newE){
+                res.push_back(newInterval);
+                copy(intervals.begin() + i, intervals.end(), back_inserter(res));
+                return res;
+            }
+            else if(intervals[i][1] < newS) res.push_back(intervals[i]);
+            else{
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+        }
+        res.push_back(newInterval);
+        return res;
+    }
+};
